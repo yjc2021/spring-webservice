160805163b5d6a8605921d63685704dbd85eb28e
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2022-05-16 16:13:52 +0900

    created project

---
+++ src/main/java/com/yjc/webservice/Application.java
@@ -0,0 +1,13 @@
+package com.yjc.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/yjc/webservice/SpringWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yjc.webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
